89a7398d8404e4ef1915a1ce0f95404a556f341e
lky1020 <57024183+lky1020@users.noreply.github.com>
2021-01-05 10:22:10 +0800

    Update && Close Project

---
+++ flutter_complete_guide/lib/main.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+import './quiz.dart';
+import './result.dart';
+
+// void main() => runApp(MyApp());
+
+void main() {
+  runApp(MyApp());
+}
+
+// StatefulWidget - Use to perform changes on the UI
+// StatelessWidget - Use to render the UI only (Click button will not change the UI)
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  final _questions = const [
+    {
+      'questionText': 'What\'s your favorite color?',
+      'answers': [
+        {'text': 'Black', 'score': 10}, 
+        {'text': 'Red', 'score': 5},
+        {'text': 'Green', 'score': 3},
+        {'text': 'White', 'score': 1},
+      ],
+    },
+    {
+      'questionText': 'What\'s your favorite animals?',
+      'answers': [
+        {'text': 'Rabbit', 'score': 10},
+        {'text': 'Snake', 'score': 5},
+        {'text': 'Elephant', 'score': 3},
+        {'text': 'Lion', 'score': 1},
+      ],
+    },
+    {
+      'questionText': 'Who\'s your favorite instructor?',
+      'answers': [
+        {'text': 'Max', 'score': 1},
+        {'text': 'Max', 'score': 1},
+        {'text': 'Max', 'score': 1},
+        {'text': 'Max', 'score': 1},
+      ],
+    },
+  ];
+
+  var _questionIndex = 0;
+  var _totalScore = 0;
+
+  void answerQuestion(int score) {
+    _totalScore += score;
+    
+    // Refresh the UI
+    setState(() {
+      _questionIndex++;
+    });
+
+    print(_questionIndex);
+
+    if (_questionIndex < _questions.length) {
+      print('We have more _questions!');
+    } else {
+      print('We have no more _questions!');
+    }
+  }
+
+  void _resetQuiz(){
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: _questionIndex < _questions.length
+            ? Quiz(
+                answerQuestion: answerQuestion,
+                questionIndex: _questionIndex,
+                questions: _questions,
+              )
+            : Result(_totalScore, _resetQuiz),
+      ),
+    );
+  }
+}
